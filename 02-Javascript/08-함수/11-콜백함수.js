67bc9083c995ee2316b96e233a0a03052cf16c95
sseungyun <gsh05144@naver.com>
2022-02-08 21:49:33 +0900

    22-02-08

--- 02-Javascript/08-함수/11-콜백함수.js
+++ 02-Javascript/08-함수/11-콜백함수.js
@@ -3,14 +3,18 @@ function something(x, y, cb) {
     // x와 y의 연산 결과를 파라미터로 전달받은 callback에게 다시 전달하여 리턴값을 받아 처리함
     const result = cb(x, y);
     console.log(x+ "와" + y + "의 연산 결과는 " + result);
-}
+    console.groupEnd();
+}   
 
+
+// 콜백함수 유형 1 - 직접 함수를 정의함
+function plus(a, b) { return a + b; }
 function minus(a, b) {return a - b; }
 
 something(100, 50, plus);
 something(100, 50, minus);
 
-// 콜백함수를 익명함수 형태로 전달하기
+// 콜백함수 유형 2 - 익명함수 형태로 전달하기
 something(200, 100, function(a, b) {
     return a * b;
 });
@@ -18,3 +22,18 @@ something(200, 100, function(a, b) {
 something(200, 100, function(a,b) {
     return a / b;
 });
+
+
+// 콜백함수 유형 3 - 익명함수를 화살표 함수로 사용
+something(5, 7, (a,b) => {
+    for(let i = a; i<b; i++) {
+        console.log("7 x " + i + " = " + (i*7));
+    }
+});
+
+// 함수 로직이 한 줄인 경우 축약된 형태
+something(5, 7, (a, b) => {
+    console.log(a + b);
+});
+
+something(5, 7, (a,b) => console.log(a + b));
---
+++ 02-Javascript/08-함수/연습문제05.js
@@ -0,0 +1,35 @@
+/**
+## 문제5 
+
+문제2번을 재귀함수 기법으로 다시 구현해 보세요.
+
+```js
+// max는 최대 행 수, current는 현재 출력중인 행의 위치
+function printRevStar(max, current=1) {
+    ... 구현하세요 ...
+}
+
+printRevStar(5);
+```
+
+ */
+
+function printStar(max, current=1) {
+    //max는 최대 행의 수, current는 현재 행의 수를 의미하므로,
+    //현재 행의 수가 최대 행보다 크다면 처리 중단을 위해 return 함수
+    if (current > max) {
+        return;
+    } else {
+        /** 한 줄을 출력하기 위한 코드 구성 */
+        let star = "";
+
+        for (let j=0; j<max-current; j++) {
+            star += "*"
+        }
+
+        console.log(star);
+        printStar(max, current+1);    // 원래는 0에서 시작했는데 1에서 시작해서 +1
+    }
+}
+
+printStar(5);
